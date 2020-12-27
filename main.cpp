32139e83bcbdf652a3d8a8b536f5de1e46604bdb
prsboy <m3616@naver.com>
2020-12-28 01:57:56 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 20;
+
+    swap(num1, num2);
+
+    cout<<num1<<" "<<num2<<endl;
+}
