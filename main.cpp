2c91d7cee359169006c8723e50813f84920ccad6
jkkim <m3616@naver.com>
2021-04-18 22:04:14 +0900

    2차원 포인터 배열

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
+/*
+    1. arr == &arr[0]
+    2. *arr == arr[0]
+    3. ptr + 1 == ptr에 sizeof(*ptr)를 더한 값
+*/
+
+
 #include <iostream>
 
-using namespace std;
-
-void printArr(int (*arr)[3])
-{
-    for(int i=0;i<2;i++)
-    {
-        
-    }
-}
+using namespace std; 
 
 int main()
 {
     int arr[2][3] = {{1,2,3},{4,5,6}};
+    
+    for(int (*row)[3] = arr; row < arr + 2; row++)
+    {
+        for(int *col = *row; col < *row + 3; col++)
+        {
+            cout<<*col<<" ";
+        }
+        cout<<endl;
+    }
 }
