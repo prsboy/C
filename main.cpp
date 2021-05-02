9a5b6d05f3d49ea85c9713fe4172ce2e5bd83a64
jkkim <m3616@naver.com>
2021-05-02 13:22:22 +0900

    동적 할당과 객체복사 문제

--- main.cpp
+++ main.cpp
@@ -1,24 +1,76 @@
-/*
-    1. arr == &arr[0]
-    2. *arr == arr[0]
-    3. ptr + 1 == ptr에 sizeof(*ptr)를 더한 값
-*/
-
-
 #include <iostream>
 
-using namespace std; 
+using namespace std;
+
+struct Point
+{
+    int x,y;
+};
+
+class Polygon
+{
+private:
+    int nPoints;
+    Point *points;
+public:
+    Polygon()
+    {
+        nPoints = 0;
+        points = NULL;
+    }
+    Polygon(const int nPoints, const Point *points) : nPoints(nPoints)
+    {
+        this->points = new Point[nPoints];
+        for(int i=0;i<nPoints;i++)
+        {
+            this->points[i] = points[i];
+        }
+    }
+
+    // 복사 생성자 구현
+    Polygon operator=(const Polygon &rhs)
+    {
+        if(this == &rhs)
+        {
+            
+        }
+        return *this;
+    }
+
+    // 이동 생성자 구현
+
+    int GetNPoints() const
+    {
+        return nPoints;
+    }
+
+    Point*GetPoints() const
+    {
+        if(nPoints == 0)
+            return NULL;
+        return points;
+    }
+};
+
+Polygon getSqare()
+{
+    Point points[4] = {{0.0}, {1,0}, {1,1}, {0,1}};
+    Polygon p(4, points);
+    return p;
+}
 
 int main()
 {
-    int arr[2][3] = {{1,2,3},{4,5,6}};
-    
-    for(int (*row)[3] = arr; row < arr + 2; row++)
+    Polygon a;
+    a = getSqare();     // 얕은 객체 복사 2회
+    Polygon b = a;     // 깊은 복사 1회
+    Polygon c;
+    c = a;      // 깊은 객체 복사 1회
+
+    int nPoints = c.GetNPoints();
+    Point *points = c.GetPoints();
+    for(int i=0;i<nPoints;i++)
     {
-        for(int *col = *row; col < *row + 3; col++)
-        {
-            cout<<*col<<" ";
-        }
-        cout<<endl;
+        cout<<"("<<points[i].x<<","<<points[i].y<<")"<<endl;
     }
 }
