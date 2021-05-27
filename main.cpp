41d25169ffef7ea15ab02b5d4be2de5c3feb4001
jkkim <m3616@naver.com>
2021-05-27 22:37:47 +0900

    상속

--- main.cpp
+++ main.cpp
@@ -1,103 +1,28 @@
-
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-struct Point {
-	int x, y;
-};
-
-class Polygon {
-public:
-	Polygon() {
-		nPoints = 0;
-		points = NULL;
-	}
-	Polygon(const int nPoints, const Point *points) : nPoints(nPoints) {
-		this->points = new Point[nPoints];
-		for (int i = 0; i < nPoints; i++)
-			this->points[i] = points[i];
-	}
-	
-	// 복사 생성자 구현
-	Polygon(const Polygon &rhs) {
-		nPoints = rhs.nPoints;
-		points = new Point[nPoints];
-		for (int i = 0; i < nPoints; i++) {
-			points[i] = rhs.points[i];
-		}
-		cout << "깊은 복사" << endl;
-	}
-
-	// 이동 생성자 구현
-	Polygon(Polygon &&rhs) {
-		nPoints = rhs.nPoints;
-		points = rhs.points;
-		rhs.points = NULL;
-		cout << "얕은 복사" << endl;
-	}
-
-	~Polygon() {
-		delete[] points;
-	}
-
-	// 복사 대입 연산자 구현
-	Polygon &operator=(const Polygon &rhs) {
-		if (this != &rhs) {
-			nPoints = rhs.nPoints;
-			delete[] points;
-			points = new Point[nPoints];
-			for (int i = 0; i < nPoints; i++) {
-				points[i] = rhs.points[i];
-			}
-		}
-
-		cout << "깊은 복사" << endl;
-		return *this;
-	}
-
-	// 이동 대입 연산자 구현
-	Polygon &operator=(Polygon &&rhs) {
-		if (this != &rhs) {
-			nPoints = rhs.nPoints;
-			delete[] points;
-			points = rhs.points;
-			rhs.points = NULL;
-		}
-		cout << "얕은 복사" << endl;
-		return *this;
-	}
-
-	int GetNPoints() const {
-		return nPoints;
-	}
-
-	Point *GetPoints() const {
-		if (nPoints == 0) return NULL;
-		return points;
-	}
-
+class TextMessage
+{
 private:
-	int nPoints;
-	Point *points;
+    int sendTime;
+    string sendName;
+    string text;
+public:
+    TextMessage(int sendTime, string sendName, string text)
+    {
+        this->sendTime = sendTime;
+        this->sendName = sendName;
+        this->text = text;
+    }
+
+    int GetSendTime() const {return sendTime;}
+    string GetSendName() const {return sendName;}
+    string GetText() const {return text;}
 };
 
-Polygon getSqare() {
-	Point points[4] = { {0, 0}, {1, 0}, {1, 1}, {0, 1} };
-	Polygon p(4, points);
-	return p;
-}
+int main()
+{
 
-int main() {
-	Polygon a;
-	a = getSqare(); // 얕은 객체 복사 2회
-	Polygon b = a; // 깊은 객체 복사 1회
-	Polygon c;
-	c = a; // 깊은 객체 복사 1회
-
-	int nPoints = c.GetNPoints();
-	Point *points = c.GetPoints();
-	for (int i = 0; i < nPoints; i++) {
-		cout << "(" << points[i].x << "," << points[i].y << ")" << endl;
-	}
 }
