2538c1943375e106a1546a3bb681bf5731fa4f0b
jkkim <m3616@naver.com>
2021-06-12 18:14:11 +0900

    상속이 필요한 이유 1

--- main.cpp
+++ main.cpp
@@ -1,36 +1,76 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class Base
+class Image
 {
 public:
-    void bFunc()
+    operator string()
     {
-        cout<<"Hello!"<<endl;
+        return "사진";
     }
-
-    int bNum;
 };
 
-class Derived : public Base
+class Message
 {
 public:
-    void dFunc()
+    Message(int sendTime, string sendName)
     {
-        cout<<"Hello?"<<endl;
+        this->sendTime = sendTime;
+        this->sendName = sendName;
     }
 
-    int dNum;
+    int GetSendTime() const {return sendTime;}
+    string GetSendName() const {return sendName;}
+private:
+    string sendName;
+    int sendTime;
+};
+
+class TextMessage : public Message
+{
+private:
+    string text;
+public:
+    TextMessage(int sendTime, string sendName, string text) : Message(sendTime, sendName)
+    {
+        this->text = text;
+    }
+
+    string GetText() const {return text;}
+};
+
+class ImageMessage : public Message
+{
+private:
+    Image *image;
+public:
+    ImageMessage(int sendTime, string sendName, Image *image) : Message(sendTime, sendName)
+    {
+        this->image = image;
+    }
+
+    Image *GetText() const {return image;}
 };
 
 int main()
 {
-    Base b;
-    Derived d;
+    Image *p_dogImage = new Image();
+    TextMessage *hello = new TextMessage(10, "두들", "안녕");
+    ImageMessage *dog = new ImageMessage(20, "두들", p_dogImage);
 
-    b.bFunc();
-    b.bNum = 1;
-
+    cout << "보낸 시간 : "<<hello->GetSendTime()<<endl;
+    cout << "보낸 사람 : "<<hello->GetSendName()<<endl;
+    cout << "  내 용   : "<<hello->GetText()<<endl;
+    cout<<endl;
     
+    cout << "보낸 시간 : "<<dog->GetSendTime()<<endl;
+    cout << "보낸 사람 : "<<dog->GetSendName()<<endl;
+    cout << "  내 용   : "<<(string)*dog->GetText()<<endl;
+    cout<<endl;
+
+    delete hello;
+    delete dog;
+    delete p_dogImage;
 }
